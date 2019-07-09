198f789f032ac67a4715b084a24b8c5a12436113
loctx-0896 <tran.xuan.loc@sun-asterisk.com>
2019-07-09 09:27:24 +0700

    login_logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,59 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   enum activated: {active: 1, unactive: 0}
   enum role: {guess: 0, admin: 1}
   has_many :orders
   has_many :contacts, dependent: :destroy
   has_many :reviews, dependent: :destroy
+  has_secure_password
+  validates :name, presence: true,
+            length: {maximum: Settings.users.name.maximum}
+  validates :email, presence: true,
+            length: {maximum: Settings.users.email.maximum},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
+  validates :password,
+    length: {minimum: Settings.users.password.minimum}, allow_nil: true
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  def authenticated? attribute, token
+    digest = send("#{attribute}_digest")
+    return false unless digest.present?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
+
+    get "/home", to: "static_pages#home"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
   end
 end
