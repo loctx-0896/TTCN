b4f966f27f823f344c6fd382196f5c7d44210215
loctx-0896 <tran.xuan.loc@sun-asterisk.com>
2019-07-30 14:31:43 +0700

    order_history

--- app/models/detail_order.rb
+++ app/models/detail_order.rb
@@ -1,3 +1,5 @@
 class DetailOrder < ApplicationRecord
   belongs_to :order
+  belongs_to :product
+  scope :find_by_order_id, ->(id){where order_id: id}
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ApplicationRecord
-  enum status: {order_success: 1, delivering: 2, delivered: 3}
+  enum status: {order_success: 1, delivered: 2}
   has_many :detail_orders, dependent: :destroy
   belongs_to :user
+  scope :sort_orders, ->{order(created_at: :desc)}
   validates :phone, presence: true
   validates :address, presence: true
   validates :name, presence: true
